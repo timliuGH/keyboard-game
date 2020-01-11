05f21b36bf1f78a838f5e7ab9e0e0314f2f06ef9
Timothy Liu <timsgithub@gmail.com>
2020-01-11 10:11:32 -0800

    Update dev page to add words and make new game page for animal alliterations

--- application.py
+++ application.py
@@ -35,11 +35,26 @@ def home():
 def game():
     """Displays game page with start/next button and current phrase"""
     category = request.args.get("category")
-    phrases = db.execute("SELECT phrase FROM phrases WHERE category=:category", {"category": category}).fetchall()
-    data = []
-    for phrase in phrases:
-        data.append(phrase[0].title())
-    return render_template("game.html", data=data, category=category)
+
+    # Handle Animal Alliterations category
+    if category == "Animal Alliterations":
+        dbAdjs = db.execute("SELECT adjective FROM adjectives").fetchall()
+        adjs = []
+        for adj in dbAdjs:
+            adjs.append(adj[0])
+        dbNouns = db.execute("SELECT noun FROM nouns").fetchall()
+        nouns = []
+        for noun in dbNouns:
+            nouns.append(noun[0])
+        return render_template("allit-game.html", adjs=adjs, nouns=nouns, category=category)
+
+    # Handle categories other than Animal Alliterations
+    else:
+        phrases = db.execute("SELECT phrase FROM phrases WHERE category=:category", {"category": category}).fetchall()
+        data = []
+        for phrase in phrases:
+            data.append(phrase[0].title())
+        return render_template("game.html", data=data, category=category)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -68,8 +83,25 @@ def dev():
         return render_template("login.html")
 
     phrases = db.execute("SELECT phrase FROM phrases").fetchall()
-    return render_template("dev.html", phrases=phrases)
+    adjectives = db.execute("SELECT adjective FROM adjectives").fetchall()
+    nouns = db.execute("SELECT noun FROM nouns").fetchall()
+
+    return render_template("dev.html", phrases=phrases, adjectives=adjectives, nouns=nouns)
 
     # Get all categories
     # categories = db.execute("SELECT category FROM phrases GROUP BY category").fetchall()
     # return render_template("dev.html", categories=categories)
+
+
+@app.route("/add-adj-n", methods=["POST"])
+def add_adj_n():
+    """Add user-supplied adjective and/or noun to database"""
+    adj = request.form.get("adjective").title()
+    noun = request.form.get("noun").title()
+    if adj != "":
+        db.execute("INSERT INTO adjectives (adjective) VALUES (:adj)", {"adj": adj})
+    if noun != "":
+        db.execute("INSERT INTO nouns (noun) VALUES (:noun)", {"noun": noun})
+    db.commit()
+
+    return redirect("/dev")
